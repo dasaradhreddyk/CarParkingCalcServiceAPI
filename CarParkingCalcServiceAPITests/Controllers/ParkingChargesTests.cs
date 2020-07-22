e59b16931cf187afb8173489280d9156a83776c4
dasaradhreddyk <58421595+dasaradhreddyk@users.noreply.github.com>
2020-07-22 19:48:46 +1000

    Update ParkingChargesTests.cs
    Test case for week end added

--- CarParkingCalcServiceAPITests/Controllers/ParkingChargesTests.cs
+++ CarParkingCalcServiceAPITests/Controllers/ParkingChargesTests.cs
@@ -15,25 +15,24 @@ namespace CarParkingCalcServiceAPI.Controllers.Tests
         {
             
             
-            ParkingCharges ctr = new ParkingCharges();
+             ParkingCharges ctr = new ParkingCharges();
             //Rule week end 
 
-            var charge = ctr.GetParkingCharge(DateTime.Now.AddHours(-9), DateTime.Now.AddHours(2));
+            var charge = ctr.GetParkingCharge(DateTime.Parse("2020-07-18 T18:00:00"), DateTime.Parse("2020-07-18T20:00:00"));
 
             //Rule early bird
-            charge =ctr.GetParkingCharge(DateTime.Now.AddHours(-9), DateTime.Now.AddHours(2));
+           // charge =ctr.GetParkingCharge(DateTime.Now.AddHours(-9), DateTime.Now.AddHours(2));
 
             //Rule night
-            charge = ctr.GetParkingCharge(DateTime.Now.AddHours(-9), DateTime.Now.AddHours(2));
+            charge = ctr.GetParkingCharge(DateTime.Parse("2020-07-01T18:00:00"), DateTime.Parse("2020-07-01T20:00:00"));
 
-              //Rule standard
+            //Rule standard
             //https://localhost:44336/ParkingCharges?CarEntryTime=2020-07-01T09:00:00&CarExistTime=2020-07-01T12:00:00
 
-            ctr.GetParkingCharge( DateTime.Parse("2020-07-01T09:00:00"), DateTime.Parse("2020-07-01T12:00:00"));
-
+           // ctr.GetParkingCharge( DateTime.Parse("2020-07-01T09:00:00"), DateTime.Parse("2020-07-01T12:00:00"));
 
             //Exception Testing. 
-            charge = ctr.GetParkingCharge(DateTime.Now.AddHours(-9),DateTime.Now.AddHours(2));
+           // charge = ctr.GetParkingCharge(DateTime.Now.AddHours(-9),DateTime.Now.AddHours(2));
 
             Assert.Fail();
         }
