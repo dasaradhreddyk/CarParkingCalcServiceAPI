6e4ef6cbe90c73e2775062184d5c542f16f23dc5
dasaradhreddyk <58421595+dasaradhreddyk@users.noreply.github.com>
2020-07-22 16:05:04 +1000

    Add files via upload

---
+++ CarParkingCalcServiceAPITests/Controllers/ParkingChargesTests.cs
@@ -0,0 +1,38 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using CarParkingCalcServiceAPI.Controllers;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using CarParkingCalcServiceAPI.Data;
+
+namespace CarParkingCalcServiceAPI.Controllers.Tests
+{
+    [TestClass()]
+    public class ParkingChargesTests
+    {
+        [TestMethod()]
+        public void GetParkingChargeTest()
+        {
+            
+            
+            ParkingCharges ctr = new ParkingCharges();
+            //Rule week end 
+
+            var charge = ctr.GetParkingCharge(DateTime.Now.AddHours(-9), DateTime.Now.AddHours(2));
+
+            //Rule early bird
+            charge =ctr.GetParkingCharge(DateTime.Now.AddHours(-9), DateTime.Now.AddHours(2));
+
+            //Rule night
+            charge = ctr.GetParkingCharge(DateTime.Now.AddHours(-9), DateTime.Now.AddHours(2));
+
+            //Rule standard
+            ctr.GetParkingCharge(DateTime.Now.AddHours(-9), DateTime.Now.AddHours(2));
+
+            //Exception Testing. 
+            charge = ctr.GetParkingCharge(DateTime.Now.AddHours(-9),DateTime.Now.AddHours(2));
+
+            Assert.Fail();
+        }
+    }
+}
